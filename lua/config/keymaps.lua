2484f034e13fd4232374dc5dcc18cf8a4076b8fd
alvaro347 <alvaro347@gmail.com>
2024-11-23 12:37:36 +0000

    keymaps, and undotree added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Sets default file explorer to leader pv
+-- Sets default file explorer to leader pv
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move selected lines up and down with J and K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor position when appending line with J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in the middle when jumping half page
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in the middle when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Yank to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
